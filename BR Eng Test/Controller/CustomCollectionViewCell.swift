d60df4a3a538114bce1e213217054673609ade67
Randy <randyvarela5@gmail.com>
2022-02-16 22:41:54 -0600

    Details view autolayout and splashsceen

--- BR Eng Test/Controller/CustomCollectionViewCell.swift
+++ BR Eng Test/Controller/CustomCollectionViewCell.swift
@@ -12,6 +12,5 @@ class CustomCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var restaurantLabel: UILabel!
     @IBOutlet weak var categoryLabel: UILabel!
-    
 }
  
